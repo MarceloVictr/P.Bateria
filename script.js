e6647ea8d39ab74d8c778ae7646add9479d336c3
Marcelo Victor <marcelomazt2@gmail.com>
2022-12-13 09:56:30 -0300

    alterando para forEach

--- script.js
+++ script.js
@@ -32,15 +32,11 @@ function playSound(sound){
 
 function playComposition(songArray) {
     let wait = 0;
-
-    for(let songItem of songArray){
-        
-        setTimeout(()=>{
-
-            playSound(`key${songItem}`);
-
-        }, wait);
-
-        wait += 250;
-    }
-}
+  
+    songArray.forEach((item) => {
+      setTimeout(() => {
+        playSound(`key${item}`);
+      }, wait);
+      wait += 250;
+    });
+  }
